3e5b5d228fa5b76b063d373219db6b10311ff59c
tejasrekhi <tejas.rekhi@nagarro.com>
2021-05-15 15:17:28 +0530

    jenkins

--- src/main/java/com/nagarro/assignment5/Assignment5Application.java
+++ src/main/java/com/nagarro/assignment5/Assignment5Application.java
@@ -4,9 +4,10 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Assignment5Application {
+public class Assignment5Application extends SpringBootServletInitializer  {
 
     public static void main(String[] args) {
 	SpringApplication application = new SpringApplication(Assignment5Application.class);
